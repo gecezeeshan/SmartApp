a08d27af2f51addc3982ea94fa2a7dbf7b7838d8
Zeeshan <gecezeeshan@gmail.com>
2021-03-06 10:36:35 +0500

    add show

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: `<p>test zeeshan page</p>
+  <div>hello</div>`, templateUrl: './app.component.html'
+  ,
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app zeeshan';
 }
---
+++ ClientApp/src/app/show/show.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'show.component',
+  templateUrl: './show.component.html'
+})
+
+
+export class show {
+  title: 'I am showing to you!!!'
+}
